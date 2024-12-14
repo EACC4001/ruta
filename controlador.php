f27c51da9fcd232dbf3a28fac5d5b60025005b12
Edgar Cruz <ecruz4996@gmail.com>
2024-12-14 09:29:00 -0600

    conexion a la base de datos

---
+++ controlador.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "rutas";
+$password = "rutasadmin";
+$dbname = "rutas";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+    die("conexion fallida: ".$conn->connect_error);
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visualizador de ubicacion y rutas</title>
+    <link href="https://api.mapbox.com/mapbox-gl-js/v3.8.0/mapbox-gl.css" rel="stylesheet">
+    <link href="style.css">
+    <style> 
+    .mapboxgl-map{height: 500px}</style>
+</head>
+<body>
+    <select id="select_dispositivo">
+        <option disabled value="0">-- Selecciona --</option>
+        <?php
+
+        ?>
+
+    </select>
+    <input type="date" id="fecha">
+    <div id="mapa"></div>
+
+    <script src="https://api.mapbox.com/mapbox-gl-js/v3.8.0/mapbox-gl.js"></script>
+    <script>
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZ29tZXo5NSIsImEiOiJjbGZiZmhwYnMwMTIwM3hsbHV6d3UwNHVnIn0.yGOMNBnMgF_LiBWvzRidpg';
+        const map = new mapboxgl.Map({
+            container: 'mapa', // container ID
+            center: [-100.814,20.52184 ], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+            zoom: 11 // starting zoom
+        });
+        const marker1 = new mapboxgl.Marker()
+        .setLngLat([-100.814,20.52184 ])
+        .addTo(map);
+    </script>
+    
+</body>
+</html>
